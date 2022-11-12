3cea2c7f92889160e6030ef8734b15d04b3c1e45
장두혁 <jjd0324@gmail.com>
2022-11-12 18:29:29 +0900

    init first Project

---
+++ vuegram/src/App.vue
@@ -0,0 +1,185 @@
+<template>
+  <div>
+<!--    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view/>
+    -->
+    <div class="header">
+      <ul class="header-button-left">
+        <li>Cancel</li>
+      </ul>
+      <div v-if="uplodeType==1">
+        <ul class="header-button-right">
+          <li @click="uplodeType=2">Next</li>
+        </ul>
+      </div>
+      <div v-if="uplodeType==2">
+        <ul class="header-button-right">
+        <li @click="publish">올리기</li>
+        </ul>
+      </div>
+      <img src="./assets/logo.png" class="logo" />
+    </div>
+
+    <PostList :demoData='demoData' :uplodeType='uplodeType' :newFileUrl='newFileUrl'/>
+    <div v-if="uplodeType == 0">
+      <button @click="more">더보기</button>
+    </div>
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input @change="upload" type="file" id="file" class="inputfile" />
+        <label for="file" class="input-plus" @click="uplodeType=1">+</label>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+import PostList from './components/PostList.vue'
+import demoData from './assets/demoData'
+import axios from 'axios'
+export default {
+  components: {
+    PostList
+  },
+  data () {
+    return {
+      sampleData: '',
+      demoData: demoData,
+      number: 0,
+      step: 0,
+      uplodeType: 0,
+      newFileUrl: '',
+      btnType: 'file'
+    }
+  },
+  setup () {
+  },
+  created () {},
+  mounted () {},
+  unmounted () {},
+  methods: {
+    // axios를 사용한 get, post 요청
+    more () {
+      axios.get(`https://codingapple1.github.io/vue/more${this.number}.json`)
+        .then((결과) => {
+          console.log(결과)
+          this.demoData.push(결과.data)
+          this.number += 1
+        })
+    },
+    // file업로드하는 방식
+    upload (e) {
+      const imageFile = e.target.files
+      // 받아온 파일을 임시 url을 만들어 저장한다.
+      const url = URL.createObjectURL(imageFile[0])
+      console.log(url)
+      this.number += 1
+      this.newFileUrl = url
+    },
+    onClickFileUploadBtn () {
+      if (this.uplodeType === 0) {
+        this.uplodeType = 1
+      } else if (this.uplodeType === 1) {
+        this.btnType = 'text'
+        this.uplodeType = 2
+      }
+    },
+    publish () {
+      const imData = {
+        name: '두혁',
+        userImage: 'https://placeimg.com/100/100/animals',
+        postImage: this.newFileUrl,
+        likes: 49,
+        date: '오늘',
+        liked: false,
+        content: '우리집 개는 화장실 물도 내림',
+        filter: 'lofi'
+      }
+      this.demoData.unshift(imData)
+      this.uplodeType = 0
+    }
+  }
+}
+</script>
+<style>
+
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
+}
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
+</style>
---
+++ vuegram/src/components/PostList.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div v-if="uplodeType == 0">
+      <div v-for="item in demoData" :key="item.id">
+        <InsPost :item='item'/>
+      </div>
+    </div>
+    <div v-if="uplodeType == 1">
+      <div class="upload-image" :style="{ backgroundImage : `url(${newFileUrl})` }" ></div>
+      <div class="filters">
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+      </div>
+    </div>
+    <div v-if="uplodeType == 2">
+      <!-- 글작성페이지 -->
+      <div class="upload-image" :style="{ backgroundImage : `url(${newFileUrl})` }"></div>
+      <div class="write">
+        <textarea class="write-box">write!</textarea>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import InsPost from './InsPost.vue'
+export default {
+  components: {
+    InsPost
+  },
+  data () {
+    return {
+      sampleData: ''
+    }
+  },
+  setup () {},
+  created () {},
+  mounted () {},
+  unmounted () {},
+  methods: {},
+  props: {
+    demoData: Array,
+    uplodeType: Number,
+    newFileUrl: String
+  }
+}
+</script>
+<style scoped>
+.upload-image{
+width: 100%;
+height: 450px;
+background: cornflowerblue;
+background-size : cover;
+}
+.filters{
+overflow-x:scroll;
+white-space: nowrap;
+}
+.filter-1 {
+width: 100px;
+height: 100px;
+background-color: cornflowerblue;
+margin: 10px 10px 10px auto;
+padding: 8px;
+display: inline-block;
+color : white;
+background-size: cover;
+}
+.filters::-webkit-scrollbar {
+height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+background: #f1f1f1;
+}
+.filters::-webkit-scrollbar-thumb {
+background: #888;
+border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+background: #555;
+}
+.write-box {
+border: none;
+width: 90%;
+height: 100px;
+padding: 15px;
+margin: auto;
+display: block;
+outline: none;
+}
+</style>
